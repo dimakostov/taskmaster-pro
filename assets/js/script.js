df68fafe0e41efd2e814748815ade3a737f9449a
Dima Kostov <dimakostov@yahoo.com>
2021-06-02 17:50:36 -0400

    added datepicker and way to check how close due date is

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -92,11 +92,19 @@ $(".list-group").on("click", "span", function(){
   .val(date);
 
   $(this).replaceWith(dateInput);
+  
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
 
   $(dateInput).trigger("focus");
+
 })
 //$(".card-header").border-radius(15%);
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
   var date = $(this)
   .val()
   .trim();
@@ -118,6 +126,8 @@ $(".list-group").on("blur", "input[type='text']", function(){
   .text(date);
 
   $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 })
   
 
@@ -229,6 +239,25 @@ $("#trash").droppable({
   }
 
 });
+
+var  auditTask = function(taskEl) {
+var date = $(taskEl).find("span").text().trim();
+
+var time = moment(date, "L").set("hour", 17);
+
+$(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+if (moment().isAfter(time)) {
+  $(taskEl).addClass("list-group-item-danger");
+}else if (Math.abs(moment().diff(time, "days")) <= 2) {
+  $(taskEl).addClass("list-group-item-warning");
+}
+
+
+}
+
+$("#modalDueDate").datepicker({ minDate: 1});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
